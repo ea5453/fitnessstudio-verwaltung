852dab87d5e8fc401fd239391b02a237795879f5
emiralici54547@gmail.com <emiralici54547@gmail.com>
2026-04-08 08:54:36 +0000

    Created Kurs_Daten, Kurs_Daten.Form1, ServerModule1 and Kurs_Daten.RowTemplate1; Edited settings; Deleted Form1

--- client_code/Kurs_Daten/__init__.py
+++ client_code/Kurs_Daten/__init__.py
@@ -1,7 +1,11 @@
-from ._anvil_designer import Form1Template
+from ._anvil_designer import Kurs_DatenTemplate
 from anvil import *
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
+import anvil.server
 
-class Form1(Form1Template):
+class Kurs_Daten(Kurs_DatenTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
